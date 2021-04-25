2c194baba5cb145f66f72e6a1264b9a3d44907f1
Andres R <zero389@gmail.com>
2021-04-25 09:43:33 -0500

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Dog Adoption Day",
+          description: "Lorem",
+          localtion: "Cali",
+          date: "January 28,2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Andres Rincon",
+        },
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Dog Adoption Day",
+          description: "Lorem",
+          localtion: "Cali",
+          date: "January 28,2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Andres Rincon",
+        },
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Dog Adoption Day",
+          description: "Lorem",
+          localtion: "Cali",
+          date: "January 28,2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Andres Rincon",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
